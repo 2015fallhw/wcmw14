df5da260d2c27303b5f86e7b2dfff58cea30b46d
kmol2016spring <kmol2016spring@example.com>
2016-06-04 10:59:38 +0800

    add viewstl.html function

--- users/g1/user1.py
+++ users/g1/user1.py
@@ -193,7 +193,13 @@ def listfiles():
     if len(files) == 0:
         return file_string + "no file!"
     for filename in files:
-        file_string += "<a href='/g1/get/"+filename+"'>"+filename+" <br />"
+        # 這裡要增加檔案副檔名的判斷, 若為 .stl, 則啟動 stl 檢視
+        #/static/viewstl.html?src=/g1/get/spur_25.stl
+        fileName, fileExtension = os.path.splitext(filename)
+        if fileExtension == ".stl":
+            file_string += "<a href='/g1/get/"+filename+"'>"+filename+"</a> (<a href='/static/viewstl.html?src=/g1/get/"+str(filename)+"'>show "+str(filename)+"</a>)<br />"
+        else:
+            file_string += "<a href='/g1/get/"+filename+"'>"+filename+"</a> <br />"
     return file_string
 @g1app.route('/get/<filename>')
 def get(filename):
@@ -204,3 +210,136 @@ def get(filename):
     # 利用導入 flask 的 current_app 取得在主應用程式中所設置的 config 變數
     download_dir = current_app.config.get('download_dir')
     return send_from_directory(download_dir, filename)
+@g1app.route('/showstl/<filename>')
+def showstl(filename):
+    outstring = '''
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>STL VIEWER ON BROWSER</title>
+<script type="text/javascript" src="jsc3d.js"></script>
+</head>
+
+<body>
+<h1>STL VIEWER</h1>
+</div>
+<div style="width:800px; margin:auto; position:relative; font-size: 9pt; color: #777777;">
+    <canvas id="cv" style="border: 1px solid;" width="800" height="600" ></canvas>
+    <div id="tip" style="display:block; color:#000000; padding:5px; position:absolute; left:10px; top:550px; background-color:#c0c0c0; height:32px; width:250px; border-radius:5px; border:1px solid #000000; font-family:Arial,sans-serif; opacity:0.5;"> 
+        Drag mouse to rotate <br> Drag mouse with shift pressed to zoom
+    </div>
+</div>
+<h2>View Edit</h2>
+<div style="width:800px;margin-left:50px;height:70px;border: 1px solid;">
+    Model Color:<input id="mdlClr" style="margin-right: 50px;" type="color" value="#00ff40" onchange="changeOption('ModelColor',this.value)"></input>
+    Back Color1:<input id="bckClr1" style="margin-right: 50px;" type="color" onchange="changeOption('BackgroundColor1',this.value)"></input>
+    Back Color2:<input id="bckClr2" type="color" onchange="changeOption('BackgroundColor2',this.value)"></input><br />
+    Render Mode:
+    <select id="render" style="margin-right: 50px;" onchange="changeOption('RenderMode',this.value)">
+        <option>Flat</option>
+        <option>Point</option>
+        <option>Wireframe</option>
+        <option>Smooth</option>
+    </select>
+    Definition:
+    <select id="defini" onchange="changeOption('Definition',this.value)">
+        <option>Standard</option>
+        <option>low</option>
+        <option>high</option>
+    </select>
+</div>
+<br />
+This web application do not support the InternetExplorer.
+
+<script type="text/javascript">
+    var viewer3d;
+    function getOptions(){
+        var mc = document.getElementById("mdlClr").value;
+        var bc1 = document.getElementById("bckClr1").value;
+        var bc2 = document.getElementById("bckClr2").value;
+        var ren = document.getElementById("render").value;
+        var def = document.getElementById("defini").value;
+        return {ModelColor: mc,
+                BackgroundColor1: bc1,
+                BackgroundColor2: bc2,
+                RenderMode: ren,
+                Definition: def};
+    }
+    function readSTLfile(opt){
+        var canvas = document.getElementById('cv');
+        viewer3d = new JSC3D.Viewer(canvas);
+        viewer3d.setParameter('SceneUrl', opt.SceneUrl);
+        viewer3d.setParameter('InitRotationX', 0);
+        viewer3d.setParameter('InitRotationY', 0);
+        viewer3d.setParameter('InitRotationZ', 0);
+        viewer3d.setParameter('ModelColor', opt.ModelColor);
+        viewer3d.setParameter('BackgroundColor1', opt.BackgroundColor1);
+        viewer3d.setParameter('BackgroundColor2', opt.BackgroundColor2);
+        viewer3d.setParameter('RenderMode', opt.RenderMode);
+        viewer3d.setParameter('Definition', opt.Definition);
+        viewer3d.init();
+        viewer3d.update();
+    }
+    function changeOption(strOpt,strValue){
+    console.log("change " + strOpt + " to " + strValue);
+        viewer3d.setParameter(strOpt, strValue);
+        viewer3d.init();
+        viewer3d.update();
+    }
+    function openByUrl(){
+        var url = document.getElementById("url").value;
+        var opt = getOptions();
+        opt.SceneUrl = document.getElementById("url").value;
+        readSTLfile(opt);
+    }
+    function openByPass(){
+        var pass = document.getElementById("url").value;
+        var opt = getOptions();
+        var fs = document.getElementById("pass").files;
+        var f = fs[0];
+        var reader = new FileReader();
+        reader.onload = (function(theFile) {
+            return function(e) {            
+                opt.SceneUrl = e.target.result;
+                console.log(opt.SceneUrl);
+                readSTLfile(opt);
+            };
+        })(f);
+        reader.readAsDataURL(f);
+    }
+    function openByGet(){
+        var q = GetQueryString();
+        console.log(q);
+        var url = q.src;
+        var opt = getOptions();
+        opt.SceneUrl = url;
+        readSTLfile(opt);
+    }
+    function GetQueryString(){
+        if( 1 < window.location.search.length )
+        {
+            var query = window.location.search.substring( 1 );
+            var parameters = query.split( '&' );
+            var result = new Object();
+            for( var i = 0; i < parameters.length; i++ )
+            {
+                var element = parameters[ i ].split( '=' );
+                var paramName = decodeURIComponent( element[ 0 ] );
+                var paramValue = decodeURIComponent( element[ 1 ] );
+                result[ paramName ] = decodeURIComponent( paramValue );
+            }
+            return result;
+        }
+        return null;
+    }
+    window.onload = function(){
+        openByGet();
+    }
+</script>
+</body>
+</html>
+'''
+    return outstring
+
